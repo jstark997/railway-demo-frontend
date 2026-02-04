fa4c715528095a3f333cbc1c9a7048364414eea5
Jeff Stark <jeffstark@Jeffs-MacBook-Pro.local>
2026-02-03 18:13:31 -1000

    Modified Dockerfile and main.jsx to change reference to VITE_API_URL to VITE_API_HOST.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 // Log the API URL for debugging
-console.log("VITE_API_URL: ", import.meta.env.VITE_API_URL);
+console.log("VITE_API_HOST: ", import.meta.env.VITE_API_HOST);
 console.log("VITE_API_PORT: ", import.meta.env.VITE_API_PORT);
 
 createRoot(document.getElementById("root")).render(
